0e34e9bf080f1f9aa07697c1e2efe1f357a234c2
svyatoslav ryumkin <svyatoslav.ryumkin@bimeister.com>
2022-01-19 23:07:00 +0300

    polish_notation

---
+++ week2/polish_notation.py
@@ -0,0 +1,38 @@
+# https://danishmujeeb.com/blog/2014/12/parsing-reverse-polish-notation-in-python/
+# Question 2
+# In reverse Polish notation for mathematical expressions, the operators follow their operands. For instance
+#
+# 3 + 4 would be written like 3 4 +
+#
+# 3 − 4 * 5 would be written like 3 4 5 * −
+#
+# To calculate reverse Polish notation expression stack data structure will be helpful. Read the expression from the left to right, if you meet operand push it into stack, otherwise execute an operation.
+#
+# Fulfil reverse\_notationreverse_notation function.
+#
+# Input format: ss is a list consisting of floats and symbols "+", "-", "*", "/".
+#
+# s = [4, 3, "-", 2, "*"]
+def reverse_notation(s):
+    stack = []
+    for i in range(len(s)):
+        if not s[i] in ['+', '-', '*', '/']:
+            # add operand to stack
+            # your code here
+            stack.append(int(s[i]))
+        else:
+            # to execute operation get operands from the stack
+            # be careful with operands order during execution
+            # your code here
+            if s[i] == "-":
+                stack.append(stack.pop() - stack.pop())
+            if s[i] == "+":
+                stack.append(stack.pop() + stack.pop())
+            if s[i] == "*":
+                stack.append(stack.pop() * stack.pop())
+            if s[i] == "/":
+                stack.append(stack.pop() / stack.pop())
+    return stack.pop()
+
+
+print(reverse_notation([10,3,5,"+","*"]))
