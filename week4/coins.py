495ba4c9b796b2e2d82774627c194998e31eb4e5
svyatoslav ryumkin <svyatoslav.ryumkin@bimeister.com>
2022-02-06 13:30:35 +0300

    coins

---
+++ week4/coins.py
@@ -0,0 +1,22 @@
+import math
+
+
+def coins(n):
+    # tbl[i] is going to contain the minimal number of coins to pay n cents
+    tbl = [math.inf] * (n + 1)
+    # tbl[0] = 0 as it should
+    tbl[0] = 0
+    for i in range(1, n + 1):
+        # we always can take a one-cent coin and pay the remaining i - 1 cents
+        tbl[i] = tbl[i - 1] + 1
+        if i >= 3:
+            # what if we can take 3-cent coin?
+            tbl[i] = min(tbl[i], tbl[i-3] + 1)
+        if i >= 7:
+            tbl[i] = min(tbl[i], tbl[i-7] + 1)
+        if i >= 8:
+            tbl[i] = min(tbl[i], tbl[i-8] + 1)
+    return tbl[n]
+
+# should print 3
+print(coins(100))
