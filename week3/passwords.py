0f592113c55af8ae0cf769ab922a5195aa1084a3
svyatoslav ryumkin <svyatoslav.ryumkin@bimeister.com>
2022-02-06 13:55:43 +0300

    passwords

--- week3/passwords.py
+++ week3/passwords.py
@@ -1,8 +1,8 @@
 # You would like to find your neighbour's password. You know its length and know that it consists only of letters A and B (your neighbor is not good in cybersecurity).
 #
-# Complete the code below to produce a recursive function passwords(n) that would produce a set of all words in letters A and B with n letters.
+# Complete the code below to produce a recursive function passwords.py(n) that would produce a set of all words in letters A and B with n letters.
 #
-# F​or example passwords(2) should return {"AA", "AB", "BA", "BB"}.
+# F​or example passwords.py(2) should return {"AA", "AB", "BA", "BB"}.
 
 def passwords(n):
     if n == 0:
